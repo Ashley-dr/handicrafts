284766e8168f2f94b3f296135b1ad552f95d597f
Joshua Magdadaro <joshuamagdada@gmail.com>
2024-09-24 17:40:16 +0800

    Login page

--- clients/app/index.tsx
+++ clients/app/index.tsx
@@ -1,37 +1,50 @@
-import { View, Text, Alert } from 'react-native'
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation' 
-import { Link } from 'expo-router'
+import { View, Text, Alert } from "react-native";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createMaterialBottomTabNavigator } from "react-native-paper/react-navigation";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import { Link } from "expo-router";
 
-import Products from './pages/Products'
-import Login from './screens/Login'
-import Signup from './screens/Signup'
-import ProfilePage from './pages/ProfilePage';
-import { getToken, verifyToken } from './AuthContext';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import Tabs from './Tabs';
+import Products from "./pages/Products";
+import Login from "./screens/Login";
+import Signup from "./screens/Signup";
+import ProfilePage from "./pages/ProfilePage";
+import { getToken, verifyToken } from "./AuthContext";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import Tabs from "./Tabs";
+import tw from "twrnc";
+import "./app.css";
 
 // const Tab = createMaterialBottomTabNavigator();
-const Tab = createMaterialBottomTabNavigator ();
-const Stack = createStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+const SignupStack = createStackNavigator();
 
-
-const LoginNav: React.FC = () => ( 
-  
-  <Tab.Navigator>
-      <Tab.Screen name="Login" component={Login} options={{title: "Log in"}}/>
-      <Tab.Screen name="Signup" component={Signup} options={{title: "Sign up"}}/>
-      
-    </Tab.Navigator>
-)
+const LoginNav: React.FC = () => (
+  <Tab.Navigator barStyle={tw`bg-[#DFC4A4] hidden`} activeColor="#4f3a3a">
+    <Tab.Screen
+      name="Login"
+      component={Login}
+      options={{
+        title: "Log in",
+        tabBarIcon: ({ color }) => (
+          <MaterialCommunityIcons name="bell" color={color} size={26} />
+        ),
+      }}
+    />
+    <Tab.Screen
+      name="Signup"
+      component={Signup}
+      options={{ title: "Sign up" }}
+    />
+  </Tab.Navigator>
+);
 
 const Home = () => {
- const [isAuth, setIsAuth] = useState<boolean | null>(null);
-   async function getData() {
+  const [isAuth, setIsAuth] = useState<boolean | null>(null);
+  async function getData() {
     try {
       const token = await getToken();
       console.log("Token:", token);
@@ -39,26 +52,24 @@ const Home = () => {
       if (token) {
         const response = await verifyToken(token);
         console.log("User Data:", response);
-        setIsAuth(response? true : false); 
+        setIsAuth(response ? true : false);
       } else {
         console.log("No token found");
         setIsAuth(false);
       }
     } catch (error) {
       console.log("Error fetching user data:", error);
-       setIsAuth(false);
+      setIsAuth(false);
     }
   }
-    useEffect(() => {
+  useEffect(() => {
     getData();
   }, []);
 
-  if(isAuth === null){
+  if (isAuth === null) {
     return null;
   }
- return isAuth ? <Tabs/> : <LoginNav/>;
- 
- 
-}
+  return isAuth ? <Tabs /> : <LoginNav />;
+};
 
-export default Home
+export default Home;
